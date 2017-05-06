c36add1167323fdcb9242d55e6baf9d80e5df881
ramkumar.murugarasan@hotmail.com <ramkumar.murugarasan@hotmail.com>
2017-05-06 12:31:26 +0530

    updated with directive name

--- src/showMore.directive.ts
+++ src/showMore.directive.ts
@@ -2,7 +2,7 @@ import { Component, Inject, ElementRef, ViewChild, HostListener, Directive, Inpu
 import { DOCUMENT } from "@angular/platform-browser";
 
 @Directive({
-  selector: 'show-more,[show-more]',
+  selector: 'ngl-show-more,[ngl-show-more]',
 })
 export class ShowMoreDirective implements OnInit, AfterViewInit {
   pseudostyle: string = "pseudoStyle" + UID.getNew();
